cf9c444ce648ee8f306bb3bfbcd9ec9b69941550
Iris <iris.thuo@student.moringaschool.com>
2022-12-11 00:05:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i<array.length; i++){
+    let pairs = target - array[i]
+  for(let k=0; i<array.length; k++){
+    if(array[k] === pairs)
+    return true
+  }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+ 0(n)
+  */
 
 /* 
   Add your pseudocode here
+    check for array
+    check if any numbers i array match up to criteria
+    return required boolean if so
+   
 */
 
 /*
   Add written explanation of your solution here
+      my function loops through the array checking for pairs that might lead to the target
+
 */
 
 // You can run `node index.js` to view these console logs
